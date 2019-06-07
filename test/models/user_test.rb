6fb44666e7029c584a558d0f83f57d83e9382f2c
Daniel Gordon <d_gordon1@icloud.com>
2019-06-07 10:37:46 +0200

    finish tdd and prepare app for travis.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "#full_name method should return first and last name capitalized" do
+    user = User.new(first_name: "jim", last_name: "bob")
+    assert_equal "Jim Bob", user.full_name
+  end
 end
